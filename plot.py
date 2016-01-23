1c73459dd3784d2cd1df531a1984a2054503c033
Pranshu Gupta <pranshug258@outlook.com>
2016-01-23 15:22:58 +0530

    Added the code for plotting the manifolds

---
+++ plot.py
@@ -0,0 +1,19 @@
+# Plot the data from a csv file containing the three Dimensional Manifold
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import sys
+
+if len(sys.argv) == 2:	
+	image_type = int(sys.argv[1])
+	# loading data from csv file
+	print "Reading manifold data from: " + 'manifold_2_' + str(image_type) + '.csv'
+	data = np.genfromtxt('manifolds/manifold_2_' + str(image_type) + '.csv', delimiter = ',', names = ['x', 'y', 'z'])
+else:
+	data = np.genfromtxt('manifolds/manifold_1.csv', delimiter = ',', names = ['x', 'y', 'z'])
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.scatter(data['x'], data['y'], data['z'])
+ax.legend()
+plt.show()
