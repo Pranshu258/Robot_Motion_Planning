69f7072763f86e0feb810e0e6cf492530726ef24
Pranshu Gupta <pranshug258@outlook.com>
2016-01-26 01:12:54 +0530

    Added the code to get the estimated co-ordinates in the manifolds

--- estimate.py
+++ estimate.py
@@ -2,12 +2,10 @@ from scipy.spatial import cKDTree
 from sklearn import manifold
 import numpy as np
 import os
-import Image
+from PIL import Image
+from operator import add
 
 learner = manifold.Isomap(n_neighbors=10, n_components=3)
-
-print "ANALYSIS OF THE RANDOM POINTS DATASET"
-print "================================================"
 
 N = 0
 img_data = []
@@ -18,9 +16,10 @@ for dirName, subdirList, fileList in os.walk(rootDir):
 		img = Image.open(dirName + "/" + fname)
 		img.load()
 		data = (np.array(img)[0]).flatten()
-		img_data.append(data)  
+		img_data.append(data) 
+		#img.close() 
 		N = N + 1
-		if N > 10000:
+		if N > 9999:
 			break 
 rootDir = 'test/img_random/'
 for dirName, subdirList, fileList in os.walk(rootDir):
@@ -29,99 +28,14 @@ for dirName, subdirList, fileList in os.walk(rootDir):
 		img = Image.open(dirName + "/" + fname)
 		img.load()
 		data = (np.array(img)[0]).flatten()
-		img_data.append(data)  
-		N = N + 1
-		if N > 10000:
-			break 
+		img_data.append(data)
+		#img.close()
 img_data = np.array(img_data)
 
 # Perform Isomap Manifold Learning
-print "Running Isomap on the dataset ..."
-data = learner.fit_transform(img_data)
-del img_data
-learned_data = data[:10000]
-test_data = data[10000:]
-del data
-
-# Find the nearest neighbor in learned data for each entry in test data
-print "Analyzing the results ..."
-dataTree = cKDTree(learned_data)
-indexes = dataTree.query(test_data)[1]
-
-coord_data = np.genfromtxt('capture_points/captured1.csv', delimiter = ',', names = ['x', 'y', 't'])
-coord_data = np.array([coord_data['x'], coord_data['y'], coord_data['t']]).T
-
-actual_data = np.genfromtxt('test/random_points.csv', delimiter = ',', names = ['x', 'y', 't'])
-actual_data = np.array([actual_data['x'], actual_data['y'], actual_data['t']]).T
-
-print "Calculating the Localization Error ..."
-i = 0
-err = 0
-for index in indexes:
-	estimated_coord = coord_data[index]
-	actual_coord = actual_data[i]
-	err = err + np.linalg.norm(actual_coord - estimated_coord)/1000
-	i = i + 1
-print "Localization Error: " + str(err)
-print "------------------------------------------------"
-
-#############################################################################################################
-
-print "ANALYSIS OF THE RADIAL TRAJECTORY DATASET"
-print "================================================"
-
-N = 0
-img_data = []
-rootDir = 'img/trajectory_3/'
-for dirName, subdirList, fileList in os.walk(rootDir):
-    print('Loading the Learning Dataset: ' + dirName)
-    for fname in fileList:
-		img = Image.open(dirName + "/" + fname)
-		img.load()
-		data = (np.array(img)[0]).flatten()
-		img_data.append(data)  
-		N = N + 1
-		if N > 10000:
-			break 
-rootDir = 'test/img_radial/'
-for dirName, subdirList, fileList in os.walk(rootDir):
-    print('Loading the Testing Dataset: ' + dirName)
-    for fname in fileList:
-		img = Image.open(dirName + "/" + fname)
-		img.load()
-		data = (np.array(img)[0]).flatten()
-		img_data.append(data)  
-		N = N + 1
-		if N > 10000:
-			break 
-img_data = np.array(img_data)
-
-# Perform Isomap Manifold Learning
-print "Running Isomap on the dataset ..."
-data = learner.fit_transform(img_data)
-del img_data
-learned_data = data[:10000]
-test_data = data[10000:]
-del data
-
-# Find the nearest neighbor in learned data for each entry in test data
-print "Analyzing the results ..."
-dataTree = cKDTree(learned_data)
-indexes = dataTree.query(test_data)[1]
-
-coord_data = np.genfromtxt('capture_points/captured3.csv', delimiter = ',', names = ['x', 'y', 't'])
-coord_data = np.array([coord_data['x'], coord_data['y'], coord_data['t']]).T
-
-actual_data = np.genfromtxt('test/radial_points.csv', delimiter = ',', names = ['x', 'y', 't'])
-actual_data = np.array([actual_data['x'], actual_data['y'], actual_data['t']]).T
-
-print "Calculating the Localization Error ..."
-i = 0
-err = 0
-for index in indexes:
-	estimated_coord = coord_data[index]
-	actual_coord = actual_data[i]
-	err = err + np.linalg.norm(actual_coord - estimated_coord)/1000
-	i = i + 1
-print "Localization Error: " + str(err)
-print "------------------------------------------------"
+print "Running Isomap on the learning dataset ..."
+train_data = learner.fit_transform(img_data[:10000])																														
+print "Running Isomap on the test dataset ..."
+test_data = learner.transform(img_data[10000:])
+print "Saving the transformed test data to test_rand_trans.csv"
+np.savetxt('test/test_rand_trans.csv', test_data, delimiter=',')
